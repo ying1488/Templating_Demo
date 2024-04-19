ef8646ed0da58c19d9a069df5f4ebbc3458f5a8f
ying1488 <yingecteach@gmail.com>
2024-04-19 17:51:58 +1000

    .

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ app.get('/', (req, res) => {
   res.render('home.ejs')
 })
 
+app.get('/r/:subreddit', (req, res) => {
+  const { subreddit } = req.params;
+  res.render('subreddit', { subreddit });
+})
+
 app.get('/rand', (req, res) => {
   const num = Math.floor(Math.random() * 10) + 1
-  res.render('random', { rand: num })
+  res.render('random', { num })
 })
 app.listen(3000, () => {
   console.log("listening on port 3000")
